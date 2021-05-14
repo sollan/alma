b68c88fb58b79879c4979338eddf0a31066ecdcb
Aljovic <67804699+Aljovic@users.noreply.github.com>
2021-05-14 14:02:58 +0200

    Update Start.py

--- Panels/Start.py
+++ Panels/Start.py
@@ -21,7 +21,7 @@ class StartPanel(wx.Panel):
         self.window_width = configs['window_width']
         self.window_height = configs['window_height']
 
-        self.header = wx.StaticText(self, -1, "DeepLimbKinematics", size=(400,60))
+        self.header = wx.StaticText(self, -1, "Automated Limb Motion Analysis (ALMA)", size=(400,60))
         font = wx.Font(20,wx.MODERN,wx.NORMAL,wx.NORMAL)
         self.header.SetFont(font)
         self.sizer.Add(self.header, pos = (0, 0), flag = wx.ALL, border = 25)
